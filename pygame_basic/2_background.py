77e099995f770c1185e0ec3ec73ba779d38568b7
Hwiyoul <blackpolarbear7@gmail.com>
2022-02-20 00:33:41 +0900

    add background work

--- pygame_basic/2_background.py
+++ pygame_basic/2_background.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((screen_width, screen_height))  # use tuple
 # screen title
 pygame.display.set_caption("Nado Gmae")
 
+# upload background image to game screen
+background = pygame.image.load("/Users/Hwiyoul/PycharmProjects/PYTHONWORKSPACE/pygame_basic/background.jpg")
+
 # event loop
 # evnet loop : monitor what users are done such as keyboard typing, mouse moving to avoid game quit
 running = True  # is game running?
@@ -17,6 +20,10 @@ while running:
     for event in pygame.event.get():  # this phrase must be written to use pygame package : what kind of events are occured?
         if event.type == pygame.QUIT:  # user quit game : the event of game screen is closed?
             running = False  # game is finished
+
+    screen.blit(background, (0, 0))  # write background image
+
+    pygame.display.update()  # draw game screen again and again
 
 # finish pygame
 pygame.quit()
