6cf0e9281463ac3bb188e1c70e218e3110e36706
Hwiyoul <blackpolarbear7@gmail.com>
2022-02-19 16:27:55 +0900

    Create new proejct
    First write

---
+++ pygame_basic/1_create_frame.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()  # initialize (initialize must be done)
+
+# setting screen size
+screen_width = 480  # horizontal size of screen
+screen_height = 640  # vertical size of screen
+screen = pygame.display.set_mode((screen_width, screen_height))  # use tuple
+
+# screen title
+pygame.display.set_caption("Nado Gmae")
+
+# event loop
+# evnet loop : monitor what users are done such as keyboard typing, mouse moving to avoid game quit
+running = True  # is game running?
+while running:
+    for event in pygame.event.get():  # this phrase must be written to use pygame package : what kind of events are occured?
+        if event.type == pygame.QUIT:  # user quit game : the event of game screen is closed?
+            running = False  # game is finished
+
+
+# finish pygame
+pygame.quit()
+
+
+
+
+
+# source : https://www.youtube.com/watch?v=Dkx8Pl6QKW0&t=0s
